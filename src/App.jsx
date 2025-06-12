264bf33474d1834d3b92e222e9341f73406f263c
Javier HM <javierhm2004@gmail.com>
2025-06-12 20:54:51 +0200

    Display 2 Timers to showcase both layouts

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import Timer from './Timer'
 
+const fechaActual = new Date();
+
+const fechaLimite1 = new Date();
+const fechaLimite2 = new Date();
+fechaLimite1.setDate(fechaActual.getDate() + 30);
+fechaLimite2.setDate(fechaActual.getDate() + 150);
+
 function App() {
   return (
-    <div className='flex bg-gray-100 min-h-screen items-center justify-center'>
-      <Timer limitDate={new Date("2025-04-15T23:38:00")}/>
+    <div className='flex bg-gray-100 min-h-screen items-center justify-center gap-20'>
+      <Timer limitDate={fechaLimite1}/>
+      <Timer limitDate={fechaLimite2}/>
     </div>
   )
 }
